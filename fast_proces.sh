51fbe9c09f53bbc5af037cae2650fe09f89a3425
Rahul Sharma <60243886+rsharma-cloudian@users.noreply.github.com>
2020-09-24 15:41:49 -0700

    fast_process.sh
    Generates a csv file that can be used to list or delete objects in a bucket when s3 bucket listing does not work.

---
+++ fast_proces.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+PROCESSED_FILE_COUNT=0
+BUCKETNAME=prophecy-recordings-replicated
+
+###CQLMD_cleansed_csv_for__* are JSON files containing parsed metadata from CLOUDIAN_TOOLS/cloudian-cass-parser
+
+for JSONFILE in `ls /root/aspect_ashs-fs2/CLEANSED_JSON/CQLMD_cleansed_csv_for__*`
+  do
+      echo "Processing json file:$JSONFILE"
+      SOURCEFILE=$(echo "$JSONFILE" |cut -d / -f 5)
+      time FC_ALL=C cat $JSONFILE | jq -r '[.Path,.Version,.DeleteMarker,.WriteTime]  | @csv' |  sed -e 's/"//g' | sed -e "s/^$BUCKETNAME\///" | rev | cut -c19- | rev >>flist_"$SOURCEFILE".csv
+      echo -en "Entries Processed from Sourcefile:"
+      FC_ALL=C wc -l flist_"$SOURCEFILE".csv
+      echo "Filtering entries written on or before::" `date -d @1597537519 --utc`
+
+      LC_ALL=C cat flist_"$SOURCEFILE".csv | awk -F ','  '{if($4 < 1597537519) print $0}' >>list_"$SOURCEFILE".out
+      echo $?
+      echo "Generated List:" list_"$SOURCEFILE".out
+      echo -en "Qualified Files:"
+      FC_ALL=C wc -l list_"$SOURCEFILE".out
+
+      (( ++PROCESSED_FILE_COUNT ))
+      echo "Processed Files: $PROCESSED_FILE_COUNT"
+ done
